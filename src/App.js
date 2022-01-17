1f9705b53976414d3b9129cf249cbc0fa406a8c0
Violetta <viart2323@gmail.com>
2022-01-17 22:22:36 +0200

    fixed bug with router

--- src/App.js
+++ src/App.js
@@ -4,21 +4,18 @@ import Header from "./Components/Header/Header";
 import NavBar from "./Components/NavBar/NavBar";
 import Profile from "./Components/Profile/MyPosts/Post/Profile";
 import Dialogs from "./Components/Dialogs/Dialogs";
-import {BrowserRouter, Route} from "react-router-dom";
+import {Route} from 'react-router-dom';
 
 const App = (props) => {
   return (
-    <BrowserRouter>
       <div className='app-wrapper'>
       <Header/>
       <NavBar/>
-
       <div className='app-wrapper-content'>
-          <Route exact  path ='/dialogs' component={Dialogs}/>
-          <Route exact path ='/profile' component={Profile}/>
+          <Route exact  path ={'/dialogs'} component={Dialogs}/>
+          <Route exact path ={'/profile'} component={Profile}/>
       </div>
     </div>
-    </BrowserRouter>
 
 );
 }
